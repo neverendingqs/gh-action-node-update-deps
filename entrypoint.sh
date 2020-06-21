8a0604162e7bd10778581eedeb3d1cac981ed2b0
Mark Tse <mark.tse@neverendingqs.com>
2020-06-20 20:21:35 -0400

    action - adding check for if files have been changed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ set -eu
 npx npm-check-updates -u
 npm i --package-lock-only
 
+if $(git diff-index --quiet HEAD); then
+  echo 'No dependencies needed to be updated!'
+  exit 0
+fi
+
 PR_BRANCH=chore/deps-$(date +%s)
 DESCRIPTION="chore: update deps (automated)"
 
