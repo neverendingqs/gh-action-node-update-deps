9fb3135983bc39f57ddf8b2c5a194ebe57684d0a
Mark Tse <mark.tse@neverendingqs.com>
2020-06-20 22:46:01 -0400

    action - add support for bumping version.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -eu
 GIT_USER_NAME=${1}
 GIT_USER_EMAIL=${2}
 PACKAGE_MANAGER=${3}
+BUMP_VERSION=${4}
 
 npx npm-check-updates -u
 
@@ -19,6 +20,14 @@ fi
 if $(git diff-index --quiet HEAD); then
   echo 'No dependencies needed to be updated!'
   exit 0
+fi
+
+if [ -n "${BUMP_VERSION}" ]; then
+  if [ "${PACKAGE_MANAGER}" == 'npm' ]; then
+    npm version ${BUMP_VERSION}
+  elif [ "${PACKAGE_MANAGER}" == 'yarn' ]; then
+    yarn version "--${BUMP_VERSION}"
+  fi
 fi
 
 DESCRIPTION="chore: update deps ($(date -I))"
