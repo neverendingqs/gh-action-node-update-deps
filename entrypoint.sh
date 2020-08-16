249f8bcec25e5b98564db41b182c1b1053681189
Mark Tse <mark.tse@neverendingqs.com>
2020-08-16 14:26:39 -0400

    feat: now uses pre-built image. (#7)
    * feat: now uses pre-built image.
    
    * fix: run npx again.
    
    * debug.
    
    * fix: assume hub is in PATH.
    
    * fix: unused var.

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,5 +40,4 @@ git checkout -b ${PR_BRANCH}
 git commit -am "${DESCRIPTION}"
 git push origin ${PR_BRANCH}
 
-curl -fsSL https://github.com/github/hub/raw/master/script/get | bash -s 2.14.1
-bin/hub pull-request -m "${DESCRIPTION}"
+hub pull-request "-m ${DESCRIPTION}"
